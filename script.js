186d218b659e486f1577b2849920d7a49fea8c91
shimul <shimul@gmail.com>
2022-02-18 07:45:58 -0800

    total updated commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function getInputValue(inputId) {
     const inputFiled = document.getElementById(inputId);
     const inputValue = parseInt(inputFiled.value);
-    inputFiled.value = '';
+
     return inputValue;
 
 }
@@ -19,10 +19,16 @@ document.getElementById('button-item').addEventListener('click', function () {
 
     const clothesItem = getInputValue('clothes-item');
 
+    //error handaling
+
+
     if (income < 0 || foodItem < 0 || rentitem < 0 || clothesItem < 0) {
         alert('Please enter the valid input!!!!');
 
     }
+    // else if (calculation > income) {
+    //     alert('Expences amount is bigger than income amount');
+    // }
     else {
 
         const calculation = foodItem + rentitem + clothesItem;
@@ -31,11 +37,49 @@ document.getElementById('button-item').addEventListener('click', function () {
         document.getElementById('total-balance').innerText = income - calculation;
 
     }
+    // if (calculation > income) {
+    //     alert('Expences amount is bigger than income amount');
+
+    // }
+
 
 
 
 
 })
+
+document.getElementById('save-button').addEventListener('click', function () {
+
+    const income = getInputValue('money-item');
+
+    const saveItem = getInputValue('saves-item');
+
+    const parsentage = (income * saveItem) / 100;
+
+    document.getElementById('savings-amount').innerText = parsentage;
+
+
+    //updated save amount
+
+    const balanceTotal = document.getElementById('total-balance');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseInt(balanceTotalText);
+
+
+    //updated remaining balance
+
+
+    if (parsentage > previousBalanceTotal) {
+        alert('Savings amount is bigger than balance');
+    } else {
+        const remaining = document.getElementById('remaining-amount');
+        const remainingText = remaining.innerText;
+        remaining.innerText = previousBalanceTotal - parsentage;
+    }
+
+})
+
+
 
 
 
