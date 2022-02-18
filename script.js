ed39d4a4b44ef31c5aabe4e5170865a3b32a35e9
shimul <shimul@gmail.com>
2022-02-18 06:17:32 -0800

    function calculation commit

--- script.js
+++ script.js
@@ -1,84 +1,44 @@
 
-// function incomeRent(income) {
-//     const moneyInput = document.getElementById('money-item');
-//     const moneyAmount = moneyInput.value;
+function getInputValue(inputId) {
+    const inputFiled = document.getElementById(inputId);
+    const inputValue = parseInt(inputFiled.value);
+    inputFiled.value = '';
+    return inputValue;
 
-// }
+}
 
 document.getElementById('button-item').addEventListener('click', function () {
-
-    const foodInput = document.getElementById('food-item');
-    const foodAmount = foodInput.value;
-
-    const rentInput = document.getElementById('rent-item');
-    const rentAmount = rentInput.value;
-
-    const clothesInput = document.getElementById('clothes-item');
-    const clothesAmount = clothesInput.value;
+    const income = getInputValue('money-item');
 
 
-    const moneyInput = document.getElementById('money-item');
-    const moneyAmount = moneyInput.value;
+    const foodItem = getInputValue('food-item');
 
 
+    const rentitem = getInputValue('rent-item');
 
 
+    const clothesItem = getInputValue('clothes-item');
 
-    let totalCost = parseInt(foodAmount) + parseInt(rentAmount) + parseInt(clothesAmount);
+    if (income < 0 || foodItem < 0 || rentitem < 0 || clothesItem < 0) {
+        alert('Please enter the valid input!!!!');
+
+    }
+    else {
+
+        const calculation = foodItem + rentitem + clothesItem;
+
+        document.getElementById('total-Expenses').innerText = calculation;
+        document.getElementById('total-balance').innerText = income - calculation;
+
+    }
 
 
-    //total cost 
-    const total = document.getElementById('total-amount');
-    const totalText = total.innerText;
-    total.innerText = totalCost;
-    //console.log(totalText);
-
-
-
-
-
-    //total balance calculation
-
-
-    let TotalBalance = parseInt(moneyAmount) - totalCost;
-
-    const calculation = document.getElementById('total-balance');
-    const calculationText = calculation.innerText;
-    calculation.innerText = TotalBalance;
-    console.log(calculationText);
 
 
 })
 
 
-document.getElementById('save-button').addEventListener('click', function () {
 
 
-    const moneyInput = document.getElementById('money-item');
-    const moneyAmount = moneyInput.value;
-
-    const saveInput = document.getElementById('saves-item');
-    const saveAmount = saveInput.value;
 
 
-    let resultMoney = (parseInt(moneyAmount) * parseInt(saveAmount)) / 100;
-
-    //updated savings balance
-    const saving = document.getElementById('savings-amount');
-    const savingText = saving.innerText;
-    saving.innerText = resultMoney;
-    console.log(saving.innerText);
-
-
-    const balanceTotal = document.getElementById('total-balance');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseInt(balanceTotalText);
-
-
-    //updated remaining balance
-    const remaining = document.getElementById('remaining-amount');
-    const remainingText = remaining.innerText;
-    remaining.innerText = previousBalanceTotal - saving.innerText;
-
-
-})
